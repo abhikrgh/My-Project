d5de6066cf8867302025520cee3b935a9efc7212
Abhishek Kumar <158467199+abhikrgh@users.noreply.github.com>
2024-08-21 04:32:36 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,31 @@
+package june7;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+		public static void main(String[] args) {
+			
+			
+			Employee e1  = new Employee() ;
+			
+			e1.setEmpId(12)  ;
+			e1.setEmpName("vishal")  ;
+			e1.setAddress("up")  ;
+			
+			
+			int iddddddd  = e1.getEmpId();
+			
+			
+			System.out.println(e1.getAddress());
+			System.out.println(e1.getEmpId());
+			System.out.println(e1);
+			
+			Employee e2  = new Employee(2323 , "Raju" , "indore") ;
+			
+			System.out.println(e2);
+			
+
+	}
+
+}
