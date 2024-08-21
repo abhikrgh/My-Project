c60768bdc2a88160901fff08dd16bbaeda52f975
Abhishek Kumar <158467199+abhikrgh@users.noreply.github.com>
2024-08-21 17:57:50 +0530

    Add files via upload

---
+++ starpat3.java
@@ -0,0 +1,21 @@
+package starpat3;
+
+public class starpat3 {
+
+	public static void main(String[] args) {
+              int count = 1;
+              for (int i=1; i<=5; i++)
+              {
+            	  for (int j=1; j<=1; j++)
+            		  
+            	  {
+            		  System.out.print(count + " ");
+            		  count++;}
+            	  {
+            	  System.out.println();
+            	  }
+              }
+
+	}
+
+}
